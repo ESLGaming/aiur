b6daf483c70821732bf7679ccc8bdedc5e13d3ea
gman-php <ich@andy-hofmann.com>
2013-06-20 13:43:17 +0200

    Fixed getter for attribute events, fixed ai-detection, key for players/observer/ai list is now the toon-handle

--- teSc2ReplayParser.py
+++ teSc2ReplayParser.py
@@ -128,7 +128,10 @@ class teSc2ReplayParser:
 
     def getAttributeEvents(self):
         if len(self.replayAttributeEvents) <= 0:
-            self.replayAttributeEvents = self.protocol.decode_replay_attributes_events(self.mpqArchive.read_file('replay.attributes.events'))
+            # This returns only a generator, we have to iterate through it to get all the events
+            attributeGenerator = self.protocol.decode_replay_attributes_events(self.mpqArchive.read_file('replay.attributes.events'))
+            for event in attributeGenerator:
+                self.replayAttributeEvents.append(event)
 
         return self.replayAttributeEvents
 
@@ -143,6 +146,17 @@ class teSc2ReplayParser:
     #  @return string The tydied string.
     def stripHtmlFromString(self, string):
         return string.replace('<sp/>', ' ')
+
+    ## Remove zero bytes (\x00) from a string.
+    #
+    #  Some strings contain zero bytes which destroys the string
+    #
+    #  @param self   The object pointer.
+    #  @param string The string to remove the zero bytes from.
+    #
+    #  @return string The tydied string.
+    def stripZeroBytesFromString(self, string):
+        return str(string.strip(u'\u0000'))
 
     ## Convert a Windows NT timestamp to a UNIX timestamp.
     #
@@ -189,10 +203,31 @@ class teSc2ReplayParser:
         playersList = self.getDetails()['m_playerList']
         for i, player in enumerate(playersList):
             # There are invalid chars (0bytes or sth) in these strings. so strip them before and convert it back from unicode- to regular string
-            player['m_toon']['m_programId'] = str(player['m_toon']['m_programId'].strip(u'\u0000'))
+            player['m_toon']['m_programId'] = self.stripZeroBytesFromString(player['m_toon']['m_programId'])
 
             playerToon = str(player['m_toon']['m_region']) + '-' + player['m_toon']['m_programId'] + '-' + str(player['m_toon']['m_realm']) + '-' + str(player['m_toon']['m_id'])
             if playerToon == toon:
+                player['m_playerId'] = i + 1
+                return player;
+
+        return None
+
+    ## Returns the player-dict for a given workingSetSlotId.
+    #
+    #  Steps through all players and immediately stops and returns the player
+    #  for the given workingSetSlotId.
+    #
+    #  @param self The object pointer.
+    #  @param toon The workingSetSlotId to find the player for.
+    #
+    #  @return dict|None The player dict or None if not found.
+    def getPlayerEntryForSlotId(self, slotId):
+        playersList = self.getDetails()['m_playerList']
+        for i, player in enumerate(playersList):
+            # There are invalid chars (0bytes or sth) in these strings. so strip them before and convert it back from unicode- to regular string
+            player['m_toon']['m_programId'] = self.stripZeroBytesFromString(player['m_toon']['m_programId'])
+
+            if player['m_workingSetSlotId'] == slotId:
                 player['m_playerId'] = i + 1
                 return player;
 
@@ -228,47 +263,53 @@ class teSc2ReplayParser:
         header = self.getHeader()
         initData = self.getInitData()
 
-        playersList = details['m_playerList']
         playersInLobby = initData['m_syncLobbyState']['m_userInitialData']
         slots = initData['m_syncLobbyState']['m_lobbyState']['m_slots']
 
-        players = {}
+        players = {'humans': {},
+                   'computers': {}}
         observers = {}
-        computers = {}
         matchWinner = -1
+        # TODO: This loop may need some refactoring to make it less complex (e.g. too much if/else)!
         for slot in slots:
-            if slot['m_userId'] == None:
-                continue;
+            # AIs don't have a userId, so we have to get the playername via the playerList
+            if slot['m_control'] == self.PLAYER_CONTROL_AI:
+                player = self.getPlayerEntryForSlotId(slot['m_workingSetSlotId'])
+                playerName = self.stripHtmlFromString(player['m_name'])
+                clanTag = ''
+                userId = -1
+                # AIs also don't have a toon, but we need them! So generate an 'invalid' one out of the playerId
+                toonHandle = '0-S2-0-' + str(player['m_playerId'])
+            elif slot['m_userId'] != None:
+                userId = slot['m_userId']
+                playerName = self.stripHtmlFromString(playersInLobby[userId]['m_name'])
+                clanTag = self.stripHtmlFromString(playersInLobby[userId]['m_clanTag'])
+                toonHandle = slot['m_toonHandle']
+            else:
+                continue
 
-            userId = slot['m_userId']
-            playerName = self.stripHtmlFromString(playersInLobby[userId]['m_name'])
-            clanTag = self.stripHtmlFromString(playersInLobby[userId]['m_clanTag']);
             data = {'user_id': userId,
                     #Some kind of a unique identifier
-                    'toon': {'handle': slot['m_toonHandle']},
+                    'toon': {'handle': toonHandle},
                     'name': playerName,
                     'clan_tag': clanTag,
                     'fullname': (('[' + clanTag + ']') if len(clanTag) > 0 else '') + playerName,
                     'team_id': slot['m_teamId']}
 
             if slot['m_observe'] > self.PLAYER_OBSERVE_IS_NO_OBSERVER:
-                observers[str(userId)] = data
-            elif slot['m_control'] == self.PLAYER_CONTROL_AI:
-                computers[str(userId)] = data
+                observers[toonHandle] = data
             else:
-                player = self.getPlayerEntryForToon(slot['m_toonHandle'])
+                player = self.getPlayerEntryForSlotId(slot['m_workingSetSlotId'])
 
                 # Something strange happend: The user is not in the playerslist!
                 if player == None:
                     continue
 
-                playerId = player['m_playerId']
-
+                # Is this the matchwinner?
                 if player['m_result'] == self.RESULT_WINNER:
-                    matchWinner = player['m_playerId']
+                    matchWinner = toonHandle
 
-                data.update({'player_id': playerId,
-                             'bnet_id': player['m_toon']['m_id'],
+                data.update({'player_id': player['m_playerId'],
                              'toon': dict(data['toon'].items() + {'programId': player['m_toon']['m_programId'],
                                                                   'region': player['m_toon']['m_region'],
                                                                   'id': player['m_toon']['m_id'],
@@ -280,7 +321,11 @@ class teSc2ReplayParser:
                                        'b': player['m_color']['m_b'],
                                        'a': player['m_color']['m_a']
                             }})
-                players[str(playerId)] = data
+
+                if slot['m_control'] == self.PLAYER_CONTROL_AI:
+                    players['computers'][toonHandle] = data
+                else:
+                    players['humans'][toonHandle] = data
 
         playersPerTeam = int(initData['m_syncLobbyState']['m_gameDescription']['m_maxUsers'] / initData['m_syncLobbyState']['m_gameDescription']['m_maxTeams'])
 
@@ -288,15 +333,14 @@ class teSc2ReplayParser:
                 'started_at': datetime.fromtimestamp(self.convertWindowsNtTimestampToUnixTimestamp(details['m_timeUTC'])).strftime('%Y-%m-%d %H:%M:%S'),
                 'utc_timezone': self.convertTimezoneOffsetToUtcTimezone(details['m_timeLocalOffset']),
                 'duration': round(header['m_elapsedGameLoops'] / 16),
-                'winner_player_id': matchWinner,
+                'winner_toon_handle': matchWinner,
                 'version': {'number': str(header['m_version']['m_major']) + '.' + str(header['m_version']['m_minor']) + '.' + str(header['m_version']['m_revision']),
                             'build': header['m_version']['m_build']},
                 'gamemode': str(playersPerTeam) + 'v' + str(playersPerTeam),
                 'gamespeed': self.gamespeeds[initData['m_syncLobbyState']['m_gameDescription']['m_gameSpeed']],
                 'host_user_id': initData['m_syncLobbyState']['m_lobbyState']['m_hostUserId'],
                 'players': players,
-                'observers': observers,
-                'computers': computers}
+                'observers': observers}
 # End class teSc2ReplayParser
 
 class UnknownBaseBuildException(Exception):
